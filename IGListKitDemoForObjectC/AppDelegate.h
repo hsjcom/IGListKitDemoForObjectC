7f73b85036435f7fdedd904f2082c6f4d8921c4d
Shaojie Hong <hsjcn@qq.com>
2017-02-13 16:56:03 +0800

    Initial Commit

---
+++ IGListKitDemoForObjectC/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  IGListKitDemoForObjectC
+//
+//  Created by Soldier on 2017/2/13.
+//  Copyright © 2017年 Shaojie Hong. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ IGListKitDemoForObjectC/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  IGListKitDemoForObjectC
+//
+//  Created by Soldier on 2017/2/13.
+//  Copyright © 2017年 Shaojie Hong. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
